c7713dec832f4f3b5c160a29f7d0dc290306047b
Aline Ramos <amr.aline@gmail.com>
2025-12-05 18:10:11 -0300

    init backend spring boot

---
+++ backend/src/main/java/com/alineramos/company_supplier_manager/CompanySupplierManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alineramos.company_supplier_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanySupplierManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanySupplierManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alineramos/company_supplier_manager/CompanySupplierManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alineramos.company_supplier_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySupplierManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
